2295f8c63f3777251ed6ee339b87dd7477be281f
Parinajain15 <parinajainindore@gmail.com>
2026-09-02 23:03:15 +0530

    Add variable fundamentals practice

---
+++ basics/variables.py
@@ -0,0 +1,35 @@
+# Q1. Create variables to store your name, age, and college.
+# Print all three.
+name = "Parina"
+age = 21
+uni = "KIIT"
+print(name,age,uni )
+
+# Q2. Create two variables:
+# x = 10
+# y = 20
+# Swap their values and print x and y.
+# Expected output: 20 10
+x = 10
+y = 20 
+x,y = y,x #multiple assignment / tuple unpacking
+print(" value of x is",x,"value of y is",y)
+
+# Q3. What will be the output?
+a = 5
+b = a
+a = 15
+print(a)
+print(b)
+#15, 5
+
+# Q4. Create a variable called 'student_name' and assign your name to it.
+# Then change its value to another name and print it.
+student_name = "Parina"
+student_name = "Ms Jain"
+print(student_name)
+
+# Q5. Create three variables in one line and assign them the values 10, 20, and 30.
+# Print all three.
+x, y, z = 10, 20, 30
+print(x,y,z)
